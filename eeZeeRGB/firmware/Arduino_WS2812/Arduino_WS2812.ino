4d5f192f0c38256e5c07bb3bc58e411404767471
shimniok@gmail.com <shimniok@gmail.com@12cc46a0-b94a-3517-bd3a-1a66fbb0d37d>
2013-11-25 21:45:33 +0000

    Removed definitions file, converted to my own ino code, updated license and comments/documentation

--- eeZeeRGB/firmware/Arduino_WS2812/Arduino_WS2812.ino
+++ eeZeeRGB/firmware/Arduino_WS2812/Arduino_WS2812.ino
@@ -1,52 +1,43 @@
-/* SparkFun WS2812 Breakout Board Example
-  SparkFun Electronics
-  date: July 25, 2013
-  license: GNU GENERAL PUBLIC LICENSE
-  
-  Requires the Adafruit NeoPixel library. It's awesome, go get it.
+/* 
+  eeZee RGB (WS2812B Breakout Board) Example
+  Michael Shimniok (www.bot-thoughts.com)
+  license: MIT License, see bottom of file
+  http://opensource.org/licenses/mit-license.php
+
+  Requires the Adafruit NeoPixel library.
   https://github.com/adafruit/Adafruit_NeoPixel
   
-  This simple example code runs three sets of animations on a group of WS2812
-  breakout boards. The more boards you link up, the better these animations
-  will look. 
-  
-  For help linking WS2812 breakouts, checkout our hookup guide:
-  https://learn.sparkfun.com/tutorials/ws2812-breakout-hookup-guide
-  
-  Before uploading the code, make sure you adjust the two defines at the
-  top of this sketch: PIN and LED_COUNT. Pin should be the Arduino pin
-  you've got connected to the first pixel's DIN pin. By default it's
-  set to Arduino pin 4. LED_COUNT should be the number of breakout boards
-  you have linked up.
+  This simple example code runs through a series of colors on a single WS2812B
+  shifting between red, green, and blue LEDs.
+
+  Hardware documentation:
+  https://code.google.com/p/bot-thoughts-eezee/wiki/eeZeeRGB
 */
 #include <Adafruit_NeoPixel.h>
 #include "WS2812_Definitions.h"
 
-#define PIN 8
-#define LED_COUNT 1
+#define PIN 8        // Arduino pin connected to WS2812B's DIN
 
-// Create an instance of the Adafruit_NeoPixel class called "leds".
-// That'll be what we refer to from here on...
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
-
-void setup()
-{
-  leds.begin();  // Call this to start up the LED strip.
-  leds.setPixelColor(0, 0);
-  leds.show();
-}
-
-#define DELAY 60
-#define MINCOLOR 0
-#define MAXCOLOR 40
+#define DELAY 60     // ms between color changes
+#define MINCOLOR 0   // lowest color setting (0-255)
+#define MAXCOLOR 40  // highest color setting (0-255)
 
 uint8_t r = MAXCOLOR; // start with red
 uint8_t g = MINCOLOR;
 uint8_t b = MINCOLOR;
 
+// New instance of NeoPixel class
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  leds.begin();
+  leds.setPixelColor(0, 0);
+  leds.show();
+}
+
 void loop()
 {
-
   // Red to Red+Blue to Blue
   for (r = MAXCOLOR, b = MINCOLOR; b < MAXCOLOR; r--, b++) {
     leds.setPixelColor(0, r, g, b);
@@ -66,3 +57,28 @@ void loop()
     delay(DELAY);
   }
 }
+
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 by Michael Shimniok
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
