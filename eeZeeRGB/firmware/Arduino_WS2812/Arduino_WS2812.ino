efec0daa9049e38c9d6fd606cce05983ec57e14a
shimniok@gmail.com <shimniok@gmail.com@12cc46a0-b94a-3517-bd3a-1a66fbb0d37d>
2013-11-25 18:51:11 +0000

    Moved electronics into separate folder, created firmware example

---
+++ eeZeeRGB/firmware/Arduino_WS2812/Arduino_WS2812.ino
@@ -0,0 +1,86 @@
+/* SparkFun WS2812 Breakout Board Example
+  SparkFun Electronics
+  date: July 25, 2013
+  license: GNU GENERAL PUBLIC LICENSE
+  
+  Requires the Adafruit NeoPixel library. It's awesome, go get it.
+  https://github.com/adafruit/Adafruit_NeoPixel
+  
+  This simple example code runs three sets of animations on a group of WS2812
+  breakout boards. The more boards you link up, the better these animations
+  will look. 
+  
+  For help linking WS2812 breakouts, checkout our hookup guide:
+  https://learn.sparkfun.com/tutorials/ws2812-breakout-hookup-guide
+  
+  Before uploading the code, make sure you adjust the two defines at the
+  top of this sketch: PIN and LED_COUNT. Pin should be the Arduino pin
+  you've got connected to the first pixel's DIN pin. By default it's
+  set to Arduino pin 4. LED_COUNT should be the number of breakout boards
+  you have linked up.
+*/
+#include <Adafruit_NeoPixel.h>
+#include "WS2812_Definitions.h"
+
+#define PIN 8
+#define LED_COUNT 1
+
+// Create an instance of the Adafruit_NeoPixel class called "leds".
+// That'll be what we refer to from here on...
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  leds.begin();  // Call this to start up the LED strip.
+  leds.setPixelColor(0, 0);
+  leds.show();
+}
+
+#define DELAY 50
+#define MINCOLOR 0
+#define MAXCOLOR 20
+
+uint8_t r = MAXCOLOR; // start with red
+uint8_t g = MINCOLOR;
+uint8_t b = MINCOLOR;
+
+void loop()
+{
+
+  // Red to Red+Blue
+  for (b = MINCOLOR; b < MAXCOLOR; b++) {
+    leds.setPixelColor(0, r, g, b);
+    leds.show();
+    delay(DELAY);
+  }
+  // Red+Blue to Blue
+  for (r = MAXCOLOR; r > MINCOLOR; r--) {
+    leds.setPixelColor(0, r, g, b);
+    leds.show();
+    delay(DELAY);
+  }
+  // Blue to Blue+Green
+  for (g = MINCOLOR; g < MAXCOLOR; g++) {
+    leds.setPixelColor(0, r, g, b);
+    leds.show();
+    delay(DELAY);
+  }
+  // Blue+Green to Green
+  for (b = MAXCOLOR; b > MINCOLOR; b--) {
+    leds.setPixelColor(0, r, g, b);
+    leds.show();
+    delay(DELAY);
+  }
+  // Green to Red+Green
+  for (r = MINCOLOR; r < MAXCOLOR; r++) {
+    leds.setPixelColor(0, r, g, b);
+    leds.show();
+    delay(DELAY);
+  }
+  // Red+Green to Red
+  for (g = MAXCOLOR; g > MINCOLOR; g--) {
+    leds.setPixelColor(0, r, g, b);
+    leds.show();
+    delay(DELAY);
+  }
+}
---
+++ eeZeeRGB/firmware/README.txt
@@ -0,0 +1,4 @@
+You'll need to get the Adafruit NeoPixel library
+
+  https://github.com/adafruit/Adafruit_NeoPixel
+
