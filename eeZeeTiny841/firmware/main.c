1afce0eefc09d185d369bc7773ba098e95f9c15e
shimniok@gmail.com <shimniok@gmail.com@12cc46a0-b94a-3517-bd3a-1a66fbb0d37d>
2014-03-01 07:16:14 +0000

    Revised to use non-reset, non-xtal pins.

--- eeZeeTiny841/firmware/main.c
+++ eeZeeTiny841/firmware/main.c
@@ -1,11 +1,21 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+/* Simple hello world blinky LED demo for ATtiny841.
+ * Michael Shimniok - www.bot-thoughts.com
+ * Assumes 16MHz crystal and CKDIV8 disabled, so
+ * HFUSE=0xdf
+ * LFUSE=0xcf
+ * EFUSE=0xff
+ */
+
+#define LED PB2
+
 int main() {
-	DDRB |= (1<<PB0);
+	DDRB |= (1<<LED);     // configure pin for output
 
 	while (1) {
-  		PINB |= (1<<PB0);
-  		_delay_ms(1000);
+  		PINB |= (1<<LED); // toggle pin
+  		_delay_ms(100);   // delay 100ms
 	}
 }
