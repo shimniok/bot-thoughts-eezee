812edb3057b2f9e65dcd79625c469fafc6ebcee8
shimniok@gmail.com <shimniok@gmail.com@12cc46a0-b94a-3517-bd3a-1a66fbb0d37d>
2014-12-02 18:39:47 +0000

    Modified prinout lines. Removed sd.end() because it (no longer?) exists.

--- eeZee_microSD/examples/Arduino/microSD_Test1/microSD_Test1.ino
+++ eeZee_microSD/examples/Arduino/microSD_Test1/microSD_Test1.ino
@@ -25,59 +25,53 @@ void setup()
 {
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+}
 
-
-  Serial.print("Initializing SD card...");
+void loop()
+{
   // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
   // Note that even if it's not used as the CS pin, the hardware SS pin 
   // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
   // or the SD library functions will not work. 
   pinMode(10, OUTPUT);
 
+  delay(5000);
+
   if (!SD.begin(10)) {
-    Serial.println("initialization failed!");
-    return;
+    Serial.println("[FAIL] initialization failed.");
+  } else {
+    Serial.println("[PASS] initialization done.");
+  
+    if (SD.exists("example.txt")) {
+      Serial.println("[FAIL] example.txt already exists.");
+    }
+    else {
+      Serial.println("[PASS] no example.txt, good.");
+    }
+  
+    // open a new file and immediately close it:
+    myFile = SD.open("example.txt", FILE_WRITE);
+    myFile.close();
+  
+    // Check to see if the file exists: 
+    if (SD.exists("example.txt")) {
+      Serial.println("[PASS] example.txt created.");
+    }
+    else {
+      Serial.println("[FAIL] example.txt not created.");  
+    }
+  
+    // delete the file:
+    SD.remove("example.txt");
+  
+    if (SD.exists("example.txt")){ 
+      Serial.println("[FAIL] example.txt removed.");
+    }
+    else {
+      Serial.println("[PASS] example.txt not removed.");  
+    }
   }
-  Serial.println("initialization done.");
-
-  if (SD.exists("example.txt")) {
-    Serial.println("example.txt exists.");
-  }
-  else {
-    Serial.println("example.txt doesn't exist.");
-  }
-
-  // open a new file and immediately close it:
-  Serial.println("Creating example.txt...");
-  myFile = SD.open("example.txt", FILE_WRITE);
-  myFile.close();
-
-  // Check to see if the file exists: 
-  if (SD.exists("example.txt")) {
-    Serial.println("example.txt exists.");
-  }
-  else {
-    Serial.println("example.txt doesn't exist.");  
-  }
-
-  // delete the file:
-  Serial.println("Removing example.txt...");
-  SD.remove("example.txt");
-
-  if (SD.exists("example.txt")){ 
-    Serial.println("example.txt exists.");
-  }
-  else {
-    Serial.println("example.txt doesn't exist.");  
-  }
-}
-
-void loop()
-{
-  // nothing happens after setup finishes.
+  
 }
 
 
