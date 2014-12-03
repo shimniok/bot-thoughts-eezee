25c94e317911bcb475cc53c7bf25ba5825640ae1
shimniok@gmail.com <shimniok@gmail.com@12cc46a0-b94a-3517-bd3a-1a66fbb0d37d>
2014-12-03 04:24:24 +0000

    Change to initialize once after reset, repeats tests every 5 secs, shows pass/fail for each.

--- trunk/eeZee_microSD/examples/Arduino/microSD_Test1/microSD_Test1.ino
+++ trunk/eeZee_microSD/examples/Arduino/microSD_Test1/microSD_Test1.ino
@@ -25,52 +25,67 @@ void setup()
 {
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-}
 
-void loop()
-{
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin 
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
-  // or the SD library functions will not work. 
+  digitalWrite(10, HIGH);
   pinMode(10, OUTPUT);
 
   delay(5000);
 
-  if (!SD.begin(10)) {
+  while(!SD.begin(10)) {
     Serial.println("[FAIL] initialization failed.");
-  } else {
-    Serial.println("[PASS] initialization done.");
-  
-    if (SD.exists("example.txt")) {
-      Serial.println("[FAIL] example.txt already exists.");
-    }
-    else {
-      Serial.println("[PASS] no example.txt, good.");
-    }
-  
-    // open a new file and immediately close it:
-    myFile = SD.open("example.txt", FILE_WRITE);
-    myFile.close();
-  
-    // Check to see if the file exists: 
-    if (SD.exists("example.txt")) {
-      Serial.println("[PASS] example.txt created.");
-    }
-    else {
-      Serial.println("[FAIL] example.txt not created.");  
-    }
-  
-    // delete the file:
-    SD.remove("example.txt");
-  
-    if (SD.exists("example.txt")){ 
-      Serial.println("[FAIL] example.txt removed.");
-    }
-    else {
-      Serial.println("[PASS] example.txt not removed.");  
-    }
+    delay(1000);
   }
+  Serial.println("[PASS] initialization done.");
+
+}
+
+void loop()
+{
+  bool pass = true;
+  
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work. 
+  if (SD.exists("example.txt")) {
+    Serial.println("[FAIL] example.txt already exists.");
+    pass = false;
+  }
+  else {
+    Serial.println("[PASS] no example.txt, good.");
+  }
+
+  // open a new file and immediately close it:
+  myFile = SD.open("example.txt", FILE_WRITE);
+  myFile.close();
+
+  // Check to see if the file exists: 
+  if (SD.exists("example.txt")) {
+    Serial.println("[PASS] example.txt created.");
+  }
+  else {
+    Serial.println("[FAIL] example.txt not created.");  
+    pass = false;
+  }
+
+  // delete the file:
+  SD.remove("example.txt");
+
+  if (SD.exists("example.txt")){ 
+    Serial.println("[FAIL] example.txt not removed.");  
+    pass = false;
+  }
+  else {
+    Serial.println("[PASS] example.txt removed.");
+  }
+
+  if (pass) {
+    Serial.println("----\n[PASS] ALL TESTS PASS\n----\n");
+  } else {
+    Serial.println("----\n[FAIL] one or more tests failed\n----\n");
+  }
+  
+  delay(5000);
   
 }
 
